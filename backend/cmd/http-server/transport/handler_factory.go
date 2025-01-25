ec292284de547b21eeb37ac467380398445eed4a
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-25 13:57:01 +0530

    Move handler factory method to transport package

---
+++ backend/cmd/http-server/transport/handler_factory.go
@@ -0,0 +1,32 @@
+package transport
+
+import (
+	"errors"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/apierror"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/handlers"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/middlewares"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/writer"
+	"log"
+	"net/http"
+)
+
+func NewHttpHandler(handler handlers.Handler, middlewares ...middlewares.Middleware) http.HandlerFunc {
+	for _, mdl := range middlewares {
+		handler = mdl(handler)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := handler(w, r); err != nil {
+			var apiErr *apierror.APIError
+			var writeErr error
+			if errors.As(err, &apiErr) {
+				writeErr = writer.ErrorJson(w, apiErr)
+			} else {
+				log.Println(err)
+				writeErr = writer.ErrorJson(w, apierror.InternalServerError)
+			}
+			if writeErr != nil {
+				log.Fatal(writeErr)
+			}
+		}
+	}
+}
--- backend/cmd/http-server/transport/handlers/types.go
+++ backend/cmd/http-server/transport/handlers/types.go
@@ -1,33 +1,7 @@
 package handlers
 
 import (
-	"errors"
-	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/apierror"
-	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/middlewares"
-	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/writer"
-	"log"
 	"net/http"
 )
 
 type Handler func(w http.ResponseWriter, r *http.Request) error
-
-func NewHttpHandler(handler Handler, middlewares ...middlewares.Middleware) http.HandlerFunc {
-	for _, mdl := range middlewares {
-		handler = mdl(handler)
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := handler(w, r); err != nil {
-			var apiErr *apierror.APIError
-			var writeErr error
-			if errors.As(err, &apiErr) {
-				writeErr = writer.ErrorJson(w, apiErr)
-			} else {
-				log.Println(err)
-				writeErr = writer.ErrorJson(w, apierror.InternalServerError)
-			}
-			if writeErr != nil {
-				log.Fatal(writeErr)
-			}
-		}
-	}
-}
