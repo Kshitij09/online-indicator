600f522d6b287ca398d966e10b3cb18be7538cac
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-25 13:57:21 +0530

    Add POST /register endpoint

--- backend/cmd/http-server/transport/server.go
+++ backend/cmd/http-server/transport/server.go
@@ -2,22 +2,28 @@ package transport
 
 import (
 	"fmt"
-	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/handlers"
 	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/middlewares"
+	"github.com/Kshitij09/online-indicator/domain"
 	"log"
 	"net/http"
 )
 
-type Server struct{}
+type Server struct {
+	domain.Storage
+}
 
-func NewServer() *Server {
-	return &Server{}
+func NewServer(storage domain.Storage) *Server {
+	return &Server{
+		Storage: storage,
+	}
 }
 func (s *Server) Run(port int) error {
 	listAddr := fmt.Sprintf(":%d", port)
 	router := http.NewServeMux()
 	logger := middlewares.HttpLogger
-	router.HandleFunc("GET /health", handlers.NewHttpHandler(health, logger))
+	router.HandleFunc("GET /health", NewHttpHandler(health, logger))
+	register := RegisterHandler(s.Storage)
+	router.HandleFunc("POST /register", NewHttpHandler(register, logger))
 	server := &http.Server{
 		Addr:    listAddr,
 		Handler: router,
