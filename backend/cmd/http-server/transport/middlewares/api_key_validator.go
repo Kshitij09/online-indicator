36c414f2092bbfbc73478be8c845f0bad1cbf041
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-25 12:28:18 +0530

    Add boilerplate transport architecture

---
+++ backend/cmd/http-server/transport/middlewares/api_key_validator.go
@@ -0,0 +1,34 @@
+package middlewares
+
+import (
+	"errors"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/apierror"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/handlers"
+	"net/http"
+	"os"
+)
+
+const apikeyHeader = "X-Api-Key"
+
+func ApiKeyValidator() (Middleware, error) {
+	serverApiKey := os.Getenv("API_KEY")
+	if serverApiKey == "" {
+		return nil, errors.New("environment variable 'API_KEY' is not set")
+	}
+	return apiKeyMiddleware(serverApiKey), nil
+}
+
+func apiKeyMiddleware(serverApiKey string) Middleware {
+	return func(next handlers.Handler) handlers.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			apiKey := r.Header.Get(apikeyHeader)
+			if apiKey == "" {
+				return apierror.SimpleAPIError(http.StatusUnauthorized, "api key is required, make sure to include 'X-Api-Key' header with a valid API key")
+			}
+			if apiKey != serverApiKey {
+				return apierror.SimpleAPIError(http.StatusForbidden, "api key is invalid")
+			}
+			return next(w, r)
+		}
+	}
+}
