0bb2a7c0cb2e8d86848e810aad7461b08c924780
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-30 19:03:41 +0530

    Add StatusService to handle ping

--- backend/domain/status.go
+++ backend/domain/status.go
@@ -10,3 +10,21 @@ type StatusDao interface {
 	IsOnline(id string) (bool, error)
 	FetchAll(ids []string) []Status
 }
+
+type StatusService struct {
+	status  StatusDao
+	session SessionDao
+}
+
+func NewStatusService(status StatusDao, session SessionDao) StatusService {
+	return StatusService{status: status, session: session}
+}
+
+func (ctx *StatusService) Ping(sessionId string) error {
+	session, err := ctx.session.Get(sessionId)
+	if err != nil {
+		return err
+	}
+	ctx.status.Update(Status{Id: session.Id, IsOnline: true})
+	return nil
+}
