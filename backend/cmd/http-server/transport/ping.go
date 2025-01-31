9e4436716529a9f94c5a8a919608e2c595e97d9a
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-31 23:31:00 +0530

    Add ping api

---
+++ backend/cmd/http-server/transport/ping.go
@@ -0,0 +1,35 @@
+package transport
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/apierror"
+	"github.com/Kshitij09/online-indicator/cmd/http-server/transport/handlers"
+	"github.com/Kshitij09/online-indicator/domain"
+	"net/http"
+)
+
+type PingRequest struct {
+	SessionId string `json:"sessionId"`
+}
+
+func PingHandler(storage domain.Storage) handlers.Handler {
+	service := domain.NewStatusService(storage.Status(), storage.Session())
+	return func(w http.ResponseWriter, r *http.Request) error {
+		var req PingRequest
+		decodeErr := json.NewDecoder(r.Body).Decode(&req)
+		if decodeErr != nil {
+			return apierror.SimpleAPIError(http.StatusBadRequest, fmt.Sprintf("invalid request: %s", decodeErr))
+		}
+		err := service.Ping(req.SessionId)
+		if errors.Is(err, domain.ErrSessionNotFound) {
+			return apierror.SimpleAPIError(http.StatusUnauthorized, fmt.Sprintf("session not found: %s", req.SessionId))
+		}
+		if err != nil {
+			return err
+		}
+		w.WriteHeader(http.StatusOK)
+		return nil
+	}
+}
