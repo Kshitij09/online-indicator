5959980f9289ec6407176842b19c6341a5921865
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-31 21:09:35 +0530

    Start caching status objects

--- backend/inmem/status.go
+++ backend/inmem/status.go
@@ -8,19 +8,19 @@ import (
 
 type StatusCache struct {
 	mu     sync.RWMutex
-	online map[string]bool
+	online map[string]domain.Status
 }
 
 func NewStatusCache() *StatusCache {
 	return &StatusCache{
-		online: make(map[string]bool),
+		online: make(map[string]domain.Status),
 	}
 }
 
 func (ctx *StatusCache) Update(status domain.Status) {
 	ctx.mu.Lock()
 	defer ctx.mu.Unlock()
-	ctx.online[status.Id] = status.IsOnline
+	ctx.online[status.Id] = status
 }
 
 func (ctx *StatusCache) FetchAll(ids []string) []domain.Status {
@@ -28,7 +28,7 @@ func (ctx *StatusCache) FetchAll(ids []string) []domain.Status {
 	defer ctx.mu.Unlock()
 	result := make([]domain.Status, 0)
 	for _, id := range ids {
-		isOnline := ctx.online[id]
+		isOnline := ctx.online[id].IsOnline
 		result = append(result, domain.Status{Id: id, IsOnline: isOnline})
 	}
 	return result
@@ -37,9 +37,9 @@ func (ctx *StatusCache) FetchAll(ids []string) []domain.Status {
 func (ctx *StatusCache) IsOnline(id string) (bool, error) {
 	ctx.mu.Lock()
 	defer ctx.mu.Unlock()
-	online, ok := ctx.online[id]
+	status, ok := ctx.online[id]
 	if ok {
-		return online, nil
+		return status.IsOnline, nil
 	} else {
 		return false, fmt.Errorf("id %v not found", id)
 	}
