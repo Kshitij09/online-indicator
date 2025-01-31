56d887e40f38fb3672701d0e049e109ad2007a46
Kshitij Patil <kshitijpatil98@gmail.com>
2025-01-31 21:46:52 +0530

    LRU list based storage

--- backend/inmem/status.go
+++ backend/inmem/status.go
@@ -1,6 +1,7 @@
 package inmem
 
 import (
+	"container/list"
 	"fmt"
 	"github.com/Kshitij09/online-indicator/domain"
 	"github.com/jonboulle/clockwork"
@@ -10,32 +11,37 @@ import (
 
 type StatusCache struct {
 	mu    sync.RWMutex
-	items map[string]*domain.Status
+	items map[string]*list.Element
+	list  *list.List
 	clock clockwork.Clock
 }
 
 func NewStatusCache(clock clockwork.Clock) *StatusCache {
 	return &StatusCache{
-		items: make(map[string]*domain.Status),
+		items: make(map[string]*list.Element),
 		clock: clock,
+		list:  list.New(),
 	}
 }
 
 func (ctx *StatusCache) UpdateOnline(id string, isOnline bool) {
 	ctx.mu.Lock()
 	defer ctx.mu.Unlock()
-	status, exists := ctx.items[id]
+	element, exists := ctx.items[id]
 	if !exists {
-		status = &domain.Status{
+		newItem := &domain.Status{
 			Id:       id,
 			IsOnline: isOnline,
 		}
+		element = ctx.list.PushBack(newItem)
+		ctx.items[id] = element
 	}
+	status := element.Value.(*domain.Status)
 	if isOnline {
 		status.LastOnline = ctx.clock.Now()
+		ctx.list.MoveToFront(element)
 	}
 	status.IsOnline = isOnline
-	ctx.items[id] = status
 }
 
 func (ctx *StatusCache) FetchAll(ids []string) []domain.Status {
@@ -43,29 +49,39 @@ func (ctx *StatusCache) FetchAll(ids []string) []domain.Status {
 	defer ctx.mu.Unlock()
 	result := make([]domain.Status, 0)
 	for _, id := range ids {
-		isOnline := ctx.items[id].IsOnline
-		result = append(result, domain.Status{Id: id, IsOnline: isOnline})
+		status, err := ctx.get(id)
+		if err != nil {
+			continue
+		}
+		result = append(result, *status)
 	}
 	return result
 }
 
 func (ctx *StatusCache) IsOnline(id string) (bool, error) {
-	ctx.mu.Lock()
-	defer ctx.mu.Unlock()
-	status, ok := ctx.items[id]
-	if ok {
-		return status.IsOnline, nil
+	status, err := ctx.get(id)
+	if err != nil {
+		return false, err
 	} else {
-		return false, fmt.Errorf("id %v not found", id)
+		return status.IsOnline, nil
 	}
 }
 
 func (ctx *StatusCache) LastOnline(id string) (time.Time, error) {
+	status, err := ctx.get(id)
+	if err != nil {
+		return time.Time{}, err
+	} else {
+		return status.LastOnline, nil
+	}
+}
+
+func (ctx *StatusCache) get(id string) (*domain.Status, error) {
 	ctx.mu.RLock()
 	defer ctx.mu.RUnlock()
-	if status, ok := ctx.items[id]; ok {
-		return status.LastOnline, nil
+	if element, ok := ctx.items[id]; ok {
+		return element.Value.(*domain.Status), nil
 	} else {
-		return time.Time{}, fmt.Errorf("id %v not found", id)
+		return nil, fmt.Errorf("id %v not found", id)
 	}
 }
