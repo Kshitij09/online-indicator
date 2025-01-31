9a916435066d4a43b9fdfb9fe533f3a970439c1b
Kshitij Patil <kshitijpatil98@gmail.com>
2025-02-01 01:22:15 +0530

    Add Status method in service

--- backend/domain/service/status.go
+++ backend/domain/service/status.go
@@ -8,11 +8,22 @@ import (
 type StatusService struct {
 	status          domain.StatusDao
 	session         domain.SessionDao
+	profile         domain.ProfileDao
 	onlineThreshold time.Duration
 }
 
-func NewStatusService(status domain.StatusDao, session domain.SessionDao, onlineThreshold time.Duration) StatusService {
-	return StatusService{status: status, session: session, onlineThreshold: onlineThreshold}
+func NewStatusService(
+	status domain.StatusDao,
+	session domain.SessionDao,
+	onlineThreshold time.Duration,
+	profile domain.ProfileDao,
+) StatusService {
+	return StatusService{
+		status:          status,
+		session:         session,
+		onlineThreshold: onlineThreshold,
+		profile:         profile,
+	}
 }
 
 func (ctx *StatusService) Ping(sessionId string) error {
@@ -22,4 +33,24 @@ func (ctx *StatusService) Ping(sessionId string) error {
 	}
 	ctx.status.UpdateOnline(session.Id, true)
 	return nil
+}
+
+func (ctx *StatusService) Status(accountId string) (domain.ProfileStatus, error) {
+	profile, exists := ctx.profile.GetByUserId(accountId)
+	if !exists {
+		return domain.EmptyProfileStatus, domain.ErrAccountNotFound
+	}
+	session, exists := ctx.session.GetByAccountId(profile.Id)
+	if !exists {
+		return domain.EmptyProfileStatus, domain.ErrSessionNotFound
+	}
+	status, err := ctx.status.Get(session.Id)
+	if err != nil {
+		return domain.EmptyProfileStatus, err
+	}
+	profileStatus := domain.ProfileStatus{
+		Profile: profile,
+		Status:  status,
+	}
+	return profileStatus, nil
 }
--- backend/domain/status.go
+++ backend/domain/status.go
@@ -8,6 +8,13 @@ type Status struct {
 	LastOnline time.Time
 }
 
+type ProfileStatus struct {
+	Profile
+	Status
+}
+
+var EmptyProfileStatus = ProfileStatus{}
+
 type StatusDao interface {
 	UpdateOnline(id string, isOnline bool)
 	Get(id string) (Status, error)
