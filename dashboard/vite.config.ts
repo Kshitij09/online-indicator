e1f99c16a16cb43222ceb3971cb0c9baed1a7378
Kshitij Patil <kshitijpatil98@gmail.com>
2025-05-03 20:20:14 +0530

    dashboard: configurable API_URL
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,15 +1,20 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [svelte()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({mode}) => {
+    // Load env file based on `mode`
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [svelte()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.API_URL || 'http://localhost:8080',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
             }
         }
     }
